61cf60069d6683aaf8766a09eba10abb45d251df
felitouuuu <159590848+felitouuuu@users.noreply.github.com>
2025-10-18 16:14:52 -0400

    Actualizar testr.js

--- testr.js
+++ testr.js
@@ -7,7 +7,7 @@ module.exports = {
 		const ROL_ID = '1390189325244829737';
 
 		const ch = msg.client.channels.cache.get(CANAL_ID) || await msg.client.channels.fetch(CANAL_ID).catch(() => null);
-		if (ch) await ch.send(`<@&${ROL_ID}> ✅ Test reinicio enviado.`).catch(() => {});
+		if (ch) await ch.send(`<@&${ROL_ID}> ✅ Test reinicio exitoso.`).catch(() => {});
 
 		await msg.reply('Test reinicio enviado.');
 	},
@@ -16,7 +16,7 @@ module.exports = {
 		const ROL_ID = '1390189325244829737';
 
 		const ch = interaction.client.channels.cache.get(CANAL_ID) || await interaction.client.channels.fetch(CANAL_ID).catch(() => null);
-		if (ch) await ch.send(`<@&${ROL_ID}> ✅ Test reinicio enviado.`).catch(() => {});
+		if (ch) await ch.send(`<@&${ROL_ID}> ✅ Test reinicio exitoso.`).catch(() => {});
 
 		await interaction.reply({ content: 'Test reinicio enviado.', ephemeral: true });
 	}
