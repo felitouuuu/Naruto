36c0fc57cbbf4e30a9590fafa17fec972f797233
felitouuuu <159590848+felitouuuu@users.noreply.github.com>
2025-11-24 11:32:13 -0500

    Actualizar crypto.js

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,1 +1,18 @@
+require('dotenv').config();
 
+// index.js
+const fs = require('fs');
+const path = require('path');
+const {
+  Client,
+  GatewayIntentBits,
+  Events,
+  Collection,
+  REST,
+  Routes,
+  EmbedBuilder
+} = require('discord.js');
+
+const CANAL_ID = '1401680611810476082';
+const ROL_ID = '1390189325244829737';
+const TOKEN = pro
