e24aa295aaf1dcecaeb7d052508acc5e87d31bd2
felitouuuu <159590848+felitouuuu@users.noreply.github.com>
2025-11-24 11:33:09 -0500

    Actualizar crypto.js

--- utils/crypto.js
+++ utils/crypto.js
@@ -1,18 +1,30 @@
-require('dotenv').config();
+const fetch = require('node-fetch');
 
-// index.js
-const fs = require('fs');
-const path = require('path');
-const {
-  Client,
-  GatewayIntentBits,
-  Events,
-  Collection,
-  REST,
-  Routes,
-  EmbedBuilder
-} = require('discord.js');
+const API_KEY = process.env.COINGECKO_API_KEY;
+const API_MODE = process.env.COINGECKO_API_MODE || 'demo';
 
-const CANAL_ID = '1401680611810476082';
-const ROL_ID = '1390189325244829737';
-const TOKEN = pro
+const COINS = {
+  btc: 'bitcoin',
+  eth: 'ethereum',
+  bnb: 'binancecoin',
+  sol: 'solana',
+  xrp: 'ripple',
+  doge: 'dogecoin'
+};
+
+async function getPrice(symbol) {
+  const id = COINS[symbol.toLowerCase()];
+  if (!id) return null;
+
+  try {
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${id}&vs_currencies=usd&x_cg_demo_api_key=${API_KEY}`;
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return data[id]?.usd || null;
+  } catch (err) {
+    return null;
+  }
+}
+
+module.exports = { getPrice, COINS };
